b5c98523a65e2644324b0d5560f434921a53a1ce
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-06-28 20:27:08 -0500

    Server.js file is working for express.

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const PORT = process.env.PORT || 3001;
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-/*// Serve up static assets (usually on heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
+/*
 // Add routes, both API and view
 app.use(routes);
 
