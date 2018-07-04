5065345f70fd91c00c4afe29dd595284eea36be8
Edgar Sandoval <ESE1307@Edgars-MacBook-Air.local>
2018-07-03 19:49:11 -0500

    Routes API and Connection

---
+++ client/database/models/users.js
@@ -0,0 +1,73 @@
+// STANDARD SEQUELIZE LIBRARY
+var Sequelize = require("sequelize");
+// CONNECTION TO DATABASE
+var sequelize = require("../config/connection");
+
+var Users = sequelize.define("users", {
+    first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,25]
+        }
+    },
+    last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,25]
+        }
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,50]
+        }
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,244]
+        }    
+    },
+    password:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [8,244]
+        } 
+    },
+    bank_name:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1,150]
+        } 
+    },
+    account_type:{
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [8]
+        } 
+    },
+    account_number:{
+        type: Sequelize.INTEGER
+    },
+    routing_number:{
+        type: Sequelize.INTEGER
+    },
+    user_closed:{
+        type: Sequelize.STRING,
+        validate: {
+            len: [1,3]
+        },
+        defaultValue: no,
+    },
+});
+
+Users.sync();
+
+module.exports = Users;
---
+++ client/database/routes/api-routes.js
@@ -0,0 +1,158 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db this are dummy routes and can be edited to match the final routes we will be using for the final project if we need any more database functionalities please let me know so I can add them or modify them
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+var Users = require ("../models/users.js");
+var Portfolio = require ("../models/portfolio.js");
+var Orders = require ("../models/orders.js");
+var Watchlist = require("../models/watchlist.js");
+// Routes
+// =============================================================
+
+module.exports = function(app) {
+
+
+    // Get All Users
+    app.get("/api/all", function(req, res){
+        Users.findAll({}).then(function(results){
+            res.json(results);
+        })
+    });
+
+    // Get Specific User By 
+    app.get("/api/all", function(req, res){
+        Users.findAll({
+            where: {
+                username: req.params.username
+            }
+        }).then(function(results){
+            res.json(results);
+        });
+    });
+
+    // Get User Portolio
+    app.get("/:user/portfolio", function(req, res){
+        Portfolio.findAll({
+            where: {
+                user_id: req.params.user_id
+            }
+        });
+    });
+    // Get All User Orders
+    app.get("/api/Orders", function(req, res){
+        Orders.findAll({
+            where: {
+                user_id: req.params.user_id
+            }
+        });
+    });
+    // Get Specific Orders Based On Date
+    app.get("/api/Orders/:date", function(req, res){
+        Orders.findAll({
+            where: {
+                order_date : req.params.order_date
+            }
+        });
+    });
+
+    // Get Specific Orders Based on Stock Name
+    app.get("/api/Orders/:stock", function(req, res){
+        Orders.findAll({
+            where: {
+                order_stock : req.params.order_stock
+            }
+        });
+    });
+
+    // Get Specific Orders Bases on Order Type
+    app.get("/api/Orders/:type", function(req, res){
+        Orders.findAll({
+            where: {
+                order_type: req.params.order_type
+            }
+        });
+    });
+
+    // Get User Watchlist
+    app.get("/api/watchlist", function(req, res){
+        Watchlist.findAll({
+            where: {
+                user_id: req.params.user_id
+            }
+        })
+    });
+
+    // Add a user
+    app.post("api/register", function(req, res){
+        console.log("User Data:");
+        console.log(req.body);
+        Users.create({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password,
+            bank_name: req.body.bank_name,
+            account_type: req.body.account_type,
+            account_number: req.body.account_number,
+            routing_number: req.body.routing_number,
+        });
+    });
+    // Add stock to watchlist
+    app.post("api/watchlist", function(req, res){
+        console.log("Watchlist Data:");
+        console.log(req.body);
+        Watchlist.create({
+            id: req.body.id,
+            user_id: req.body.user_id,
+            watch_stock: req.body.watch_stock
+        });
+    });
+    // Add stock to portfolio
+    app.post("api/portfolio", function(req, res){
+        console.log("User Data: ");
+        console.log(req.body);
+        Portfolio.create({
+            portfolio_id: req.body.portfolio_id,
+            user_id: req.body.user_id,
+            portfolio_value: req.body.portfolio_value,
+            buying_power: req.body.buying_power,
+            cash_ready: req.body.cash_ready,
+            owned_stock: req.body.owned_stock
+        });
+
+    });
+    // Add order to Order list
+    app.post("api/register", function(req, res){
+        console.log("User Data: ");
+        console.log(req.body);
+        Orders.create({
+            order_id: req.body.first_name,
+            customer_id: req.body.last_name,
+            portfolio_id: req.body.username,
+            order_date: req.body.email,
+            order_shares: req.body.password,
+            order_amount: req.body.bank_name,
+            stock_name: req.body.account_type,
+        });
+    });
+    // DELETE route for deleting items from watchlist
+    app.post("/api/posts/:id", function(req, res) {
+        Watchlist.destroy({
+            where: {
+            id: req.params.id
+            }
+        })
+    });
+    // DELETE route for deleting stocks from portfolio
+    app.post("/api/port/:id", function(req, res) {
+        Portfolio.destroy({
+            where: {
+            owned_stock: req.params.owned_stock
+            }
+        })
+    });
+}
