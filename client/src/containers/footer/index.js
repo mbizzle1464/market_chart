4903118714f58e1155e4606b7680730c3d510fda
Jared Gilpin <irishkungfu@gmail.com>
2018-07-07 12:04:10 -0500

    update styling on news section as well as login page

--- client/src/containers/footer/index.js
+++ client/src/containers/footer/index.js
@@ -1,15 +1,14 @@
 import React from "react";
 
 export default () => (
-  <div className="widget">
-    <h1>Footer Goes Here</h1>
-    <div className="search-container">
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate non
-        quaerat ipsam nihil adipisci quam, molestias beatae recusandae nam
-        voluptas explicabo corrupti alias ipsum, cumque error? Culpa fugiat
-        reprehenderit obcaecati?
-      </p>
-    </div>
+  <div className="widget footer">
+    <ul>
+      <li>Home</li>
+      <li>About</li>
+      <li>Portfolio</li>
+      <li>News</li>
+    </ul>
+    <div className="">Middle Part</div>
+    <div className="">Right Side</div>
   </div>
 );
--- database/config/connection.js
+++ database/config/connection.js
@@ -3,12 +3,11 @@
 // *********************************************************************************
 
 // Dependencies
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize('sequelize_test', 'root', 'P@ssw0rd', {
-    
-  host: 'localhost',
+const sequelize = new Sequelize("sequelize_test", "root", "P@ssWord", {
+  host: "localhost",
   dialect: "mysql",
   operatorsAliases: false,
   pool: {
@@ -16,7 +15,7 @@ const sequelize = new Sequelize('sequelize_test', 'root', 'P@ssw0rd', {
     min: 0,
     acquire: 30000,
     idle: 10000
-  },
+  }
 });
 
 // Exports the connection for other files to use
