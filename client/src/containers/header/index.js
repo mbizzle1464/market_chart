7466695432e54722ee95b487f43e70a812e88303
Jared Gilpin <irishkungfu@gmail.com>
2018-07-12 13:16:23 -0500

    header update

--- client/src/containers/header/index.js
+++ client/src/containers/header/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
 
+let test = localStorage.getItem("aws-amplify-cacheCurSize");
 class Header extends React.Component {
   render() {
     return (
       <header>
         <div className="logo-container">
           <Link to="/">
-            <img height="80px" src="/images/logo/logo.svg" />
+            <img height="100px" src="/images/logo/logo.svg" />
           </Link>
         </div>
         <nav>
@@ -25,6 +26,7 @@ class Header extends React.Component {
             <li className="nav-list__item">
               <Link to="/signout">Sign Out</Link>
             </li>
+            <li>{test}</li>
           </ul>
         </nav>
       </header>
