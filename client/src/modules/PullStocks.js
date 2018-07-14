ae6fff07a7eebfd30b5cdc66035af2de8afc4539
Jared Gilpin <irishkungfu@gmail.com>
2018-07-13 19:05:40 -0500

    setup api for my portfolio

--- client/src/modules/PullStocks.js
+++ client/src/modules/PullStocks.js
@@ -17,7 +17,8 @@ export const RECEIVE_CHART = "PullStocks/RECEIVE_CHART";
 export const RELOAD_COMPANY = "PullStocks/RELOAD_COMPANY";
 export const RECEIVE_PORTFOLIO = "PullStocks/RECEIVE_PORTFOLIO";
 export const TOGGLE_MODAL = "PullStocks/TOGGLE_MODAL";
-
+export const RECEIVE_PORTFOLIO_DESCRIPTION =
+  "PullStocks/RECEIVE_PORTFOLIO_DESCRIPTION";
 const initialState = {
   quote: [],
   price: "",
@@ -31,7 +32,8 @@ const initialState = {
   companyReceived: false,
   currentCompany: "",
   modal: false,
-  portfolio: [{}]
+  portfolio: [{}],
+  receivedPortfolioIex: false
 };
 
 // export default (state = initialState, action) => {
@@ -90,13 +92,30 @@ export default (state = initialState, action) => {
           companyReceived
         };
       }
+    case RECEIVE_PORTFOLIO_DESCRIPTION:
+      if (!action.payload) {
+        return [...state];
+      } else {
+        init();
+        let test = action.payload[0];
+        let portfolio = state.portfolio;
+        portfolio.portfolio.stocks.map((stock, index) => {
+          stock.currentPrice =
+            action.payload[0][stock.symbol].quote.latestPrice;
+        });
+        let receivedPortfolioIex = true;
+        return {
+          ...state,
+          portfolio,
+          receivedPortfolioIex
+        };
+      }
     case RECEIVE_PORTFOLIO:
       if (!action.payload) {
         return [...state];
       } else {
         init();
         let portfolio = action.payload[0];
-        portfolio.portfolio.stocks[0].currentPrice = 2;
         let awaitingPortfolio = true;
         return {
           ...state,
@@ -525,6 +544,15 @@ export const getPortfolio = user_id => async dispatch => {
   dispatch({
     type: RECEIVE_PORTFOLIO,
     payload: [res.data]
+  });
+};
+export const getDescriptionPortfolio = symbols => async dispatch => {
+  const response = await axios.get(
+    `${url}market/batch?symbols=${symbols}&types=quote,news,chart&range=1m&last=5`
+  );
+  dispatch({
+    type: RECEIVE_PORTFOLIO_DESCRIPTION,
+    payload: [response.data]
   });
 };
 
