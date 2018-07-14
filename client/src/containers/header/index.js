e05e4685c5fa79341d42f8e458df0f338e83a3e4
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-07-14 11:59:57 -0500

    Updated files.

--- client/src/containers/header/index.js
+++ client/src/containers/header/index.js
@@ -1,9 +1,20 @@
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-
-let test = localStorage.getItem("aws-amplify-cacheCurSize");
+let user = localStorage.getItem(`CognitoIdentityServiceProvider.37oeqm62ij2n2g5opttuhveunt.LastAuthUser`)
+let login = localStorage.getItem(`CognitoIdentityServiceProvider.37oeqm62ij2n2g5opttuhveunt.${user}.accessToken`);
 class Header extends React.Component {
+
+    state = {
+      login: login
+    }
+    switchState = (signOut) => {
+      this.setState({
+        signOut
+      })
+      //console.log(signOut);
+    }
   render() {
+    const { signOut } = this.state
     return (
       <header>
         <div className="logo-container">
@@ -23,10 +34,17 @@ class Header extends React.Component {
             <li className="nav-list__item">
               <Link to="/news">News</Link>
             </li>
-            <li className="nav-list__item">
-              <Link to="/signout">Sign Out</Link>
-            </li>
-            <li>{test}</li>
+              {
+              signOut ? (
+                <li className="nav-list__item">
+                <Link to="/auth">Sign In</Link>              
+              </li>
+              ) : (
+                <li className="nav-list__item">
+                <Link to="/signout">Sign Out</Link>
+              </li>
+              )
+            }
           </ul>
         </nav>
       </header>
