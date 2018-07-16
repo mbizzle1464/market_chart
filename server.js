37ae2fcd48b11c6ca2fea27dce54c01288eea9f4
Michael Bizzle <mbizzle@studiomoviegrill.com>
2018-07-16 11:14:56 -0500

    Updated files.

--- server.js
+++ server.js
@@ -14,9 +14,6 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 // Add routes, both API and view
 // app.use(routes);
 require("./database/routes/portfolio.js")(app);
